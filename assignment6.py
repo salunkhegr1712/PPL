bf68b6df0a69765c4975bff7379389e3b6276fe0
salunkhegr1712 <69299458+salunkhegr1712@users.noreply.github.com>
2020-11-11 12:11:14 +0530

    Add files via upload

---
+++ assignment6.py
@@ -0,0 +1,63 @@
+import turtle as tt
+
+s = turtle.getscreen()
+t = turtle.Turtle()
+
+
+class shape:
+    def __init__(x, sides=0, length=0):
+        x.sides = sides
+        x.length = length
+
+
+class polygon(shape):
+
+    def info(x):
+        print("A polygon can be defined as a flat or plane, two-dimensional vwith straight sides.")
+
+
+class square(polygon):
+    def show(x):
+        t.fd(x.length)
+        t.rt(90)
+        t.fd(x.length)
+        t.rt(90)
+        t.fd(x.length)
+        t.rt(90)
+        t.fd(x.length)
+        t.rt(90)
+
+
+class pentagon(polygon):
+    def show(x):
+        for i in range(5):
+            t.forward(x.length)
+            t.right(72)
+
+
+class hexagon(polygon):
+    def show(x):
+        for i in range(6):
+            t.forward(x.length)
+            t.right(60)
+
+
+class octagon(polygon):
+    def show(x):
+        for i in range(6):
+            t.forward(x.length)
+            t.right(45)
+
+
+class triangle(polygon):
+    def show(x):
+        t.forward(x.length)
+        t.left(120)
+        t.forward(x.length)
+        t.left(120)
+        t.forward(x.length)
+
+
+hex1 = hexagon(6, 100)
+hex1.info()
+hex1.show()
